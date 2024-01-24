e15924b996047c30a7a4a62bedefbc968cfe5022
dipen kukadiya <dipenkukadiya@gmail.com>
2024-01-24 17:14:37 +0530

    dad

--- src/main/java/com/example/Coder/Entity/User.java
+++ src/main/java/com/example/Coder/Entity/User.java
@@ -11,6 +11,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -21,12 +24,16 @@ public class User {
     @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @NotBlank(message = "Username must not be blank")
     @Column(name = "user_name")
     private String Username;
 
+    @Email(message = "Invalid email format")
     @Column(name = "email")
     private String email;
 
+    @Size(min = 10, message = "Password must be at least 10 characters long")
     @Column(name = "password")
     private String password;
 
