4e44aeb76f7de68ac4a5f779b9b77618102f6bd6
dipen kukadiya <dipenkukadiya@gmail.com>
2024-02-01 11:54:34 +0530

    hi

--- src/main/java/com/example/Coder/Service/impl/CustomUserDetailsService.java
+++ src/main/java/com/example/Coder/Service/impl/CustomUserDetailsService.java
@@ -1,44 +1,44 @@
-package com.example.Coder.Service.impl;
+// package com.example.Coder.Service.impl;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.core.userdetails.UsernameNotFoundException;
+// import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+// import java.util.Collection;
+// import java.util.Set;
+// import java.util.stream.Collectors;
 
 
+// import org.springframework.security.core.GrantedAuthority;
+// import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import com.example.Coder.Entity.Role;
-import com.example.Coder.Entity.User;
-import com.example.Coder.Repository.UserRepo;
-@Service
-public class CustomUserDetailsService implements UserDetailsService {
-    @Autowired
-    UserRepo userRepo;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepo.findByUsername(username);
 
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                getAuthorities(user.getRoles())
-        );
-    }
+// import com.example.Coder.Entity.Role;
+// import com.example.Coder.Entity.User;
+// import com.example.Coder.Repository.UserRepo;
+// @Service
+// public class CustomUserDetailsService implements UserDetailsService {
+//     @Autowired
+//     UserRepo userRepo;
 
-    private Collection<? extends GrantedAuthority> getAuthorities(Set<Role> roles) {
-        return roles.stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getRoleName()))
-                .collect(Collectors.toList());
-    }
-}
+//     @Override
+//     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//         User user = userRepo.findByUsername(username);
+
+//         return new org.springframework.security.core.userdetails.User(
+//                 user.getUsername(),
+//                 user.getPassword(),
+//                 getAuthorities(user.getRoles())
+//         );
+//     }
+
+//     private Collection<? extends GrantedAuthority> getAuthorities(Set<Role> roles) {
+//         return roles.stream()
+//                 .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getRoleName()))
+//                 .collect(Collectors.toList());
+//     }
+// }
